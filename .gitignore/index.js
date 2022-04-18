789a60f09b7ecdcdf0963c459d1e97da2c9eba11
Guiguizandou <47714626+Guiguizandou@users.noreply.github.com>
2022-04-18 22:31:50 +0200

    Update index.js
    Ajouts de deux autres radios

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,8 @@ bot.on('ready', () => {
 
 bot.on('message', function (message) {
     if (message.content === '=help') {
-        message.channel.send({embed: {color: 5483232, title: 'Voici les commandes disponibles :', description: "=help : Voir toutes les commandes\n =radio-on : Allumer NG-Radio\n =radio-off : Éteindre NG-Radio"}})
+        message.channel.send({embed: {color: 5483232, title: 'Voici les commandes disponibles :',
+                                      description: "=help : Voir toutes les commandes\n =radio-ng : Allumer NG-Radio\n =radio-funradio : Allumer Funradio\n =radio-mouv : Allumer Mouv'\n =radio-off : Éteindre la radio"}})
     }
 })
 
@@ -17,7 +18,7 @@ bot.on('message', async message => {
   // we ignore it
   if (!message.guild) return;
 
-  if (message.content === '=radio-on') {
+  if (message.content === '=radio-ng') {
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join();
@@ -27,6 +28,44 @@ bot.on('message', async message => {
       });
     } else {
       message.reply('Vous devez être connecté à un channel pour mettre NG-Radio');
+    }
+  }
+});
+
+bot.on('message', async message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '=radio-funradio') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      console.log('Radio On');
+      const dispatcher = connection.play('http://streaming.radio.funradio.fr/fun-1-44-128?listen=webCwsBCggNCQgLDQUGBAcGBg', {
+        volume: 0.5,
+      });
+    } else {
+      message.reply('Vous devez être connecté à un channel pour mettre Funradio');
+    }
+  }
+});
+
+bot.on('message', async message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '=radio-mouv') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      console.log('Radio On');
+      const dispatcher = connection.play('http://direct.mouv.fr/live/mouv-midfi.mp3', {
+        volume: 0.5,
+      });
+    } else {
+      message.reply('Vous devez être connecté à un channel pour mettre Mouv\'');
     }
   }
 });
